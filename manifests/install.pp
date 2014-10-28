bfc7ced4f3195895e371776b3b3567a47b49d23f
Ubuntu <ubuntu@ip-10-0-0-229.eu-central-1.compute.internal>
2014-10-28 06:39:38 +0000

    add aws to beaker

--- manifests/install.pp
+++ manifests/install.pp
@@ -5,4 +5,22 @@ class logclient::install {
   package { $logclient::package_name:
     ensure => present,
   }
+  $config_hash = {
+    'START'        => 'true',
+    'LS_GROUP'     => 'root',
+    'LS_USER'      => 'root',
+    'LS_HOME'      => '/opt/logstash',
+    'HOME'         => '/opt/logstash',
+    'SINCEDB_DIR'  => '/opt/logstash/tmp',
+  }
+
+  class { 'logstash':
+    manage_repo     => true,
+    repo_version    => '1.4',
+    status          => 'enabled',
+    init_defaults   => $config_hash,
+    java_install    => true,
+    install_contrib => true,
+  }
+
 }
